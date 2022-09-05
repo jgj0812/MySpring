60ecb8477edc76c4a6bb8dc47af2a425a3c3d1d7
warsong <jgj0812@outlook.com>
2022-09-05 11:50:19 +0900

    commit3

--- src/main/java/com/example/springboot/study/Application.java
+++ src/main/java/com/example/springboot/study/Application.java
@@ -1,2 +1,43 @@
-package com.example.springboot.study;public class Application {
+package com.example.springboot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * A001
+ * @SpringBootApplication
+ *  스프링 부트의 자동 설정
+ *  스프링 Bean 읽기와 생성 모두 자동으로 설정
+ *  이 어노테이션이 있는 위치부터 설정을 읽는다.
+ *  프로젝트의 최 상단에 위치!!
+ *
+ *  main() 에서 프로그램이 시작된다.
+ *  내장된(built-in) WAS(Web Application Server)로 Tomcat 을 기본으로 지원
+ *
+ *  Controller 를 만든다.
+ *
+ *  File - Settings -Editor - General - Code Completion -> Match Case 해제
+ * 
+ *  A004
+ *      전체 프로그램 한번 실행해보기
+ *
+ *          웹서버가 동작하고 확인을 위해
+ *          http://localhost:8080/hello
+ *          했을 때, 화면에 hello 가 나와야 정상
+ *
+ *      Class 와 DB를 매핑시켜준다.
+ *          Class - Getter, Setter 를 자동으로 처리해주는 라이브러리 설치 : Lombok
+ *          롬복
+ *              Getter, Setter, Default Constructor, toString() 등을 Annotation 으로 자동 생성
+ *
+ *              Ctrl + Shift + a => plugins 에서 installed 에서 Lombok 확인
+ *
+ *              File > Settings > Build, Executing, Depl... > Complier > Annotation Proccessor
+ * */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/com/example/springboot/study/web/HelloController.java
+++ src/main/java/com/example/springboot/study/web/HelloController.java
@@ -1,2 +1,66 @@
-package com.example.springboot.study.web;public class HelloController {
+package com.example.springboot.study.web;
+
+/*
+ * A002
+ * RESTful API
+ *
+ *  http://test.com/main.jsp?name=test&age=12
+ *  http://test.com/mian.jsp/user/2
+ *  http://test.com/hello
+ *
+ *  METHOD
+ *      GET     : HTTP Header 정보가 날아간다.
+ *          http://test.com/login.jsp?id=test&pass=1234
+ *          1. 보안상의 심각한 문제 발생
+ *          2. 메시지 길이가 정해져있다. (255B)
+ *
+ *          http://test.com/hello
+ *
+ *          http://test.com/show.jsp?id=3
+ *      POST    : HTTP Body에 정보가 날아간다.
+ *
+ *      DELETE
+ *      PUT
+ *
+ *  @RestController
+ *      JSON(Javascript Object Notation)로 반환하는 컨트롤로 만들때
+ *
+ *  HTTP Response Code
+ *  1xx : Trying
+ *          ------------> 100
+ *          <------------ 180 Ringing
+ *  2xx : OK
+ *  3xx : Redirect Error, Temporary Error
+ *  4xx : Permanent Error, Client Error
+ *          404 Not Found, 403 Forbidden, 405 Method Not Allowed
+ *  5xx : Server Error
+ *          Gateway Error
+ *  6xx : Global Error
+ *
+ *  Spring boot 장점
+ *      1. Class, DB 매핑
+ *      2. Database 변경할 때 포팅이 편하다.
+ *      3. 단위테스트 @Test를 사용
+ *          HelloController.java
+ *          HelloControllerTest.java
+ * */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello()
+    {
+        return "hello";
+    }
+
+    @GetMapping("test")
+    public String test()
+    {
+        return "테스트를 결과로 준다.";
+    }
+
 }
